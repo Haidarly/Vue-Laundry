de131f4be4d214a63e601808e84d1df928e9966a
Karuyama20 <animers.go20@gmail.com>
2022-03-24 09:46:05 +0700

    first up commit

---
+++ src/components/Template/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <button class="btn btn-sm btn-danger btn-icon-split" @click="logout">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-sign-out-alt"></i>
+                    </span>
+                    <span class="text">Logout</span>
+                </button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    created() {
+        this.axios.get('http://localhost/projek_laundry/public/api/login/check', {
+            headers : {'Authorization' : 'bearer' + this.$store.state.token}
+        })
+        .then( (res) => {
+            if(!(res.data.success)) {
+                this.$store.commit('clearToken')
+                this.$store.commit('clearUser')
+                this.$swal("Error","Sesi Login Sudah Berakhir","Error")
+                this.$router.push('/login')
+            }
+        })
+    },
+    methods : {
+        logout() {
+            this.axios.post('http://localhost/projek_laundry/public/api/logout', { headers : {'Authorization' : 'Bearer' + this.$store.state.token}}).then( () => {
+                this.$store.commit('clearToken')
+                this.$store.commit('clearUser')
+                this.$router.push('/login')
+            })
+        }
+    }
+}
+</script>
---
+++ src/components/Transaksi/Index.vue
@@ -0,0 +1,91 @@
+<template>
+    <div id="wrapper">
+        <sidebar-component></sidebar-component>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <navbar-component></navbar-component>
+
+                <div class="container-fluid">
+                    <h1 class="h3 mb-4 text-gray-800">Data Transaksi</h1>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card shadow mb-4">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <router-link to="/transaksi/tambah" class="btn btn-info btn-icon-split">
+                                            <span class="icon text-white-50">
+                                                <i class="fas fa-plus"></i>
+                                            </span>
+                                            <span class="text">Tambah</span>
+                                        </router-link>
+                                        <table class="table table-bordered mt-3" width="100%" cellspacing="0">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Nama Member</th>
+                                                    <th>Tanggal Order</th>
+                                                    <th>Status</th>
+                                                    <th>Aksi</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr v-for="(t, index) in transaksi" :key="index">
+                                                    <td>{{ index + 1}}</td>
+                                                    <td>{{ t.nama }}</td>
+                                                    <td>{{ t.tgl_order | moment("DD/MM/YYYY") }}</td>
+                                                    <td>
+                                                        <span v-if="t.status == 'baru'" class="badge bg-info text-light">Baru</span>
+                                                        <span v-if="t.status == 'proses'" class="badge bg-warning text-dark">Proses</span>
+                                                        <span v-if="t.status == 'selesai'" class="badge bg-success text-light">Selesai</span>
+                                                        <span v-if="t.status == 'diambil'" class="badge bg-secondary text-light">Diambil</span>
+                                                    </td>
+                                                    <td>
+                                                        <router-link :to="{ name : 'detailtransaksi' , params : { id : t.id }}" class="btn btn-success btn-circle mr-1">
+                                                            <i class="far fa-eye"></i>
+                                                        </router-link>
+                                                        <!-- <router-link :to="{ name : 'edittransaksi' , params : { id : t.id }}" class="btn btn-warning btn-circle mr-1">
+                                                            <i class="fas fa-pen"></i>
+                                                        </router-link> -->
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer-component></footer-component>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            transaksi : {}
+        }
+    },
+    created() {
+        var data = JSON.parse(this.$store.state.datauser)
+        var role = data.role
+
+        if (role == 'owner'){
+            this.$swal("Error","Anda tidak dapat mengakses halaman ini","error")
+            this.$router.push('/') 
+        }
+
+        this.axios.get('http://localhost/projek_laundry/public/api/transaksi',
+                        {
+                            headers : {Authorization : 'Bearer' + this.$store.state.token}
+                        })
+                    .then(res => {
+                        this.transaksi = res.data.data
+                    })
+                    .catch(err => console.log(err))
+    },
+}
+</script>
